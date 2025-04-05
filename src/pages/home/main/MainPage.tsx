08431fb178125702da2ed59af11fda59146440b9
bruhmagedon <schubert.walk@yandex.ru>
2025-04-05 11:51:49 +0300

    градиент

--- src/pages/home/main/MainPage.tsx
+++ src/pages/home/main/MainPage.tsx
@@ -24,22 +24,23 @@ const _MainPage = () => {
   return (
     <main className='flex flex-1 flex-col gap-12 px-6 sm:px-16 h-full items-center mt-5 overflow-hidden'>
       <div className='relative w-full rounded-[0.375rem]'>
-        <BlockNoteView theme={theme} editor={editor} />
+        <BlockNoteView theme={theme as Theme} editor={editor} />
       </div>
-      
-      <div style={{
-  
-        border: '0.125rem solid transparent',
-        background: `
+
+      <div
+        style={{
+          border: '0.125rem solid transparent',
+          background: `
           linear-gradient(#232325, #232325) padding-box,
-          radial-gradient(circle, #bfffcb,#26ae6f,#85fb97) border-box
+          radial-gradient(circle, #047118,#0c8334,#0b931f) border-box
         `,
-        borderRadius: '1rem' // Для круга
-      }}
-      className='flex relative max-w-fit  inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full  z-[5000] px-2 py-2  items-center justify-center space-x-4'>
-        <Button>Отправить на проверку</Button>
-        <Button>Экспортировать в PDF</Button>
-        <Button>Копировать текст</Button>
+          borderRadius: '1rem' // Для круга
+        }}
+        className='flex relative max-w-fit inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full z-[5000] px-2 py-2 items-center justify-center space-x-4'
+      >
+        <Button className='rounded-md'>Отправить на проверку</Button>
+        <Button className='rounded-md'>Экспортировать в PDF</Button>
+        <Button className='rounded-md'>Копировать текст</Button>
       </div>
     </main>
   );
