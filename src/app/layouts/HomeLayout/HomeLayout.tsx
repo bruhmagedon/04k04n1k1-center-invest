6581352fad128f9b66fdd40e049b055e7a4c3bfb
vsevolod22 <96574851+vsevolod22@users.noreply.github.com>
2025-04-05 10:17:27 +0300

    new

--- src/app/layouts/HomeLayout/HomeLayout.tsx
+++ src/app/layouts/HomeLayout/HomeLayout.tsx
@@ -1,11 +1,7 @@
 import { Outlet } from 'react-router';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/shared/ui/tabs';
+import { Tabs } from '@/shared/ui/tabs';
 
-import { CircleUserRound, Copy, Notebook, ScrollText, Search, SquarePlus } from 'lucide-react';
-import { Button } from '@/shared/ui/button';
 
-import logoSvg from '@/shared/assets/logo.svg';
-import { ThemeSwitcher } from '@/widgets/ThemeSwitcher/ThemeSwitcher';
 import { LeftSideBar } from '@/app/layouts/HomeLayout/components/LeftSideBar';
 import { HomeHeader } from '@/app/layouts/HomeLayout/components/HomeHeader';
 export const HomeLayout = () => {
@@ -16,10 +12,11 @@ export const HomeLayout = () => {
       <Tabs defaultValue='Редактор' className='w-full bg-[#232325] rounded-tr-xl'>
       <HomeHeader></HomeHeader>
         <main>
-          <TabsContent value='Редактор'>Make changes to your account here.</TabsContent>
-          <TabsContent value='Проверка'>Change your password here.</TabsContent>
+          {/* <TabsContent value='Редактор'>Make changes to your account here.</TabsContent>
+          <TabsContent value='Проверка'>Change your password here.</TabsContent> */}
+          <Outlet/>
         </main>
-
+        
       </Tabs>
     </div>
   );
